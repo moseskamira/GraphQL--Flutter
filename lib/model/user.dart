0de2f46b702607a1f53b92d9bae8ad87dd9bcef1
moseskamira <moses.african@gmail.com>
2025-03-02 19:37:26 +0300

    Improve Users Screen

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,24 +1,22 @@
 import 'package:json_annotation/json_annotation.dart';
 
+part 'user.g.dart';
+
 @JsonSerializable()
 class User {
   String? id;
   String? name;
+  String? email;
+  String? phone;
 
   User({
     this.id,
     this.name,
+    this.email,
+    this.phone,
   });
 
-  User.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    name = json['name'];
-  }
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['name'] = this.name;
-    return data;
-  }
+  Map<String, dynamic> toJson() => _$UserToJson(this);
 }
